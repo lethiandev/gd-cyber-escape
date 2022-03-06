6dee0ebeca911e2bed0767688a205d6ca482a4eb
Konrad Nowakowski <konrad.x92@gmail.com>
2022-03-06 14:58:20 +0000

    Add background tileset

--- src/stages/basement/polygon_outline.gd
+++
@@ -1,7 +0,0 @@
-extends StaticBody2D
-
-func _draw() -> void:
-	var color = Color("1e405b")
-	for child in get_children():
-		if child is Polygon2D:
-			draw_polyline(child.polygon, color, 4.0, true)
